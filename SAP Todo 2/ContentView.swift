25fd64e5798af2290780100c36480dc7749def16
ReyRachel8L <rlim.rachel08@gmail.com>
2022-07-02 09:37:34 +0800

    Created checkbox and strike through.

--- SAP Todo 2/ContentView.swift
+++ SAP Todo 2/ContentView.swift
@@ -12,15 +12,21 @@ struct ContentView: View {
     var todos = [
         Todo(title: "Work on my skill issues"),
         Todo(title: "Watch some Paw Patrol episodes"),
+        Todo(title: "Delete everyone's points"),
         Todo(title: "Gotten hot chocolate", isCompleted: true)
     ]
     
     var body: some View {
         NavigationView {
             List(todos) { todo in
-                Text(todo.title)
+                HStack {
+                    Image(systemName: todo.isCompleted ? "shield.fill"
+                          : "shield")
+                    Text(todo.title)
+                        .strikethrough(todo.isCompleted)
+                }
             }
-            .navigationTitle("TD list")
+            .navigationTitle("Paw Patrol Todo")
         }
     }
 }
